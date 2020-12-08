b8e053cf4dca255333fc399e06dff08f4fc3e2f8
davien21 <chidiebereekennia@gmail.com>
2020-12-08 12:45:54 +0100

    add handleLike method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-
+import Like from "./like";
 class Movies extends Component {
   state = {
     movies: getMovies(),
@@ -13,6 +13,16 @@ class Movies extends Component {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
   };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
   render() {
     const { length: count } = this.state.movies;
 
@@ -28,6 +38,7 @@ class Movies extends Component {
               <th scope="col">Genre</th>
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
+              <th scope="col"></th>
               <th />
             </tr>
           </thead>
@@ -38,6 +49,9 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
@@ -53,6 +67,8 @@ class Movies extends Component {
       </React.Fragment>
     );
   }
+
+  toggleLike() {}
 }
 
 export default Movies;
