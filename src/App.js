6ad6265c7c849e9c519c770b79c071452d6d3e6e
davien21 <chidiebereekennia@gmail.com>
2020-12-26 23:34:16 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Movies from './components/movies';
+import NavBar from './components/navbar';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import MovieForm from './components/movieForm';
+import LoginForm from './components/loginform';
 import './App.css';
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies"></Redirect>
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+      
+    </React.Fragment>
   );
 }
 
