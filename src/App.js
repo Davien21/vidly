0ef75677cef4995f9181a1d738565073e085bf3b
davien21 <chidiebereekennia@gmail.com>
2020-12-27 13:59:18 +0100

    rename loginform component to loginForm

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import MovieForm from './components/movieForm';
-import LoginForm from './components/loginform';
+import LoginForm from './components/loginForm';
 import './App.css';
+import RegisterForm from './components/registerForm';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
           <Route path="/login" component={LoginForm}></Route>
+          <Route path="/register" component={RegisterForm}></Route>
           <Route path="/not-found" component={NotFound}></Route>
           <Redirect from="/" exact to="/movies"></Redirect>
           <Redirect to="/not-found" />
